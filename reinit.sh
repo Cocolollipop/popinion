21529b6f27a8e0308b5a4005b9d85c483f480362
Zdravko IVANOVIC <zdravko.ivanovic.info@gmail.com>
2016-04-10 23:47:00 +0200

    Adds DataFixtures

--- reinit.sh
+++ reinit.sh
@@ -1,4 +1,6 @@
-php bin/console doctrine:database:create
-php bin/console doctrine:schema:update --force
+php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:schema:drop --force
+php bin/console doctrine:schema:create
+php bin/console doctrine:fixtures:load -n
 php bin/console cache:clear
 php bin/console assets:install
